702188866b1192f177925335ee3472d99f319f07
[dyad] <cledera.ernie@gmail.com>
2026-07-02 19:01:50 +0800

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-<Routes>
-  <Route path="/" element={<Index />} />
-  <Route path="/splash" element={<SplashScreen />} />
-  <Route path="*" element={<NotFound />} />
-</Routes>
+import { Routes, Route } from "react-router-dom";
+
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import SplashScreen from "./components/SplashScreen";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/splash" element={<SplashScreen />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default App;
