fbf9a6ba45b5ed6ee9eafbee90d404c37d64aa40
[dyad] <cledera.ernie@gmail.com>
2026-07-02 19:05:29 +0800

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./globals.css";
+import { DarkVeilProvider } from "./components/layout/DarkVeilProvider";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <DarkVeilProvider>
+      <App />
+    </DarkVeilProvider>
   </BrowserRouter>
 );
